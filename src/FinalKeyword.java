a40bae16456341114ffb7dcee607e0572b46a481
Etienne Gwiavander <gwiavanderetienne2@gmail.com>
2024-07-16 05:47:07 +0100

    final keyword in java

---
+++ src/FinalKeyword.java
@@ -0,0 +1,11 @@
+public class FinalKeyword {
+    public static void main(String[] args) {
+        // anything that is declared as final cannot be changed/updated along the line in the program
+        // used for constants 
+
+        final double PI = 3.14159;
+
+        System.out.println(PI);
+
+    }
+}
