0aa99b2d6731ce4b5a23eb1eb81bed0e6d44a607
Etienne Gwiavander <gwiavanderetienne2@gmail.com>
2024-08-15 06:04:57 +0100

    converting the (int)data to (char)data

--- src/OOP/fileReader/Main.java
+++ src/OOP/fileReader/Main.java
@@ -1,5 +1,6 @@
 package OOP.fileReader;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -9,41 +10,42 @@ public class Main {
         // FileReader = read the contents of a file as a stream of characters. one by one
         //              read() returns an int value which contains the byte value
         //              When read() returns -1, there is no more data to be read
-        //              To read the actual content, we need to convert these ASCII values back to characters.
-        
-        try (FileReader reader = new FileReader("C:\\Users\\ETIENNE G\\Desktop\\JavaProject\\src\\OOP\\fileReader\\test.txt")) {
-            int data;
-            while ((data = reader.read()) != -1) {
-                char c = (char) data;
-                System.out.print(c);
+
+        try {
+            FileReader reader = new FileReader("C:\\Users\\ETIENNE G\\Desktop\\JavaProject\\src\\OOP\\fileReader\\test.txt");
+            int data = reader.read();
+            while (data != -1) {
+//              System.out.print((int)data); // converting the (int)data to (char)data makes it posible for the actual characters of ASCII code to be read instead of it's integer
+                System.out.print((char)data);
+                data = reader.read();
             }
-        } catch (IOException e) {
+            reader.close();
+        }catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
-        }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        
+        } 
     }
 }
 
 // public class Main {
 //     public static void main(String[] args) {
-        // FileReader = read the contents of a file as a stream of characters. one by one
-        //              read() returns an int value which contains the byte value
-        //              When read() returns -1, there is no more data to be read
-
-//         try {
-//             FileReader reader = new FileReader("C:\\Users\\ETIENNE G\\Desktop\\JavaProject\\src\\OOP\\fileReader\\test.txt");
-//             int data = reader.read();
-//             while (data != -1) {
-//                 System.out.print((int)data);
-//                 data = reader.read();
-//             }
-//             reader.close();
-//         }catch (FileNotFoundException e) {
-//             // TODO Auto-generated catch block
-//             e.printStackTrace();
-//         } catch (IOException e) {
-//             // TODO Auto-generated catch block
-//             e.printStackTrace();
+//         // FileReader = read the contents of a file as a stream of characters. one by one
+//         //              read() returns an int value which contains the byte value
+//         //              When read() returns -1, there is no more data to be read
+//         //              To read the actual content, we need to convert these ASCII values back to characters.
         
-//         } 
+//         try (FileReader reader = new FileReader("C:\\Users\\ETIENNE G\\Desktop\\JavaProject\\src\\OOP\\fileReader\\test.txt")) {
+//             int data;
+//             while ((data = reader.read()) != -1) {
+//                 char c = (char) data;
+//                 System.out.print(c);
+//             }
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
 //     }
 // }
